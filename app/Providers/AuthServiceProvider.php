3c031c8c7a90936a54aafdcee7d1c93460f31e53
chuanchuan1 <1126371945@qq.com>
2018-08-10 16:49:23 +0000

    Only authorized user can edit user profile

--- app/Providers/AuthServiceProvider.php
+++ app/Providers/AuthServiceProvider.php
@@ -14,6 +14,7 @@ class AuthServiceProvider extends ServiceProvider
      */
     protected $policies = [
         'App\Model' => 'App\Policies\ModelPolicy',
+        \User\Model\User::class => \App\Policies\UserPolicy::class
     ];
 
     /**

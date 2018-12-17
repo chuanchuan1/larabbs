d7dfab4142d8c7aff11f47d0c72e54eb2ba0e6df
chuanchuan1 <1126371945@qq.com>
2018-12-17 17:19:39 +0000

    新增几个天气接口

--- app/Http/Controllers/Api/WeatherController.php
+++ app/Http/Controllers/Api/WeatherController.php
@@ -8,6 +8,7 @@ use Illuminate\Http\Request;
 class WeatherController extends Controller
 {
     protected $weather;
+
     public function __construct()
     {
         $this->weather = new Weather(config("openapi.weather.appkey"));
@@ -19,6 +20,59 @@ class WeatherController extends Controller
 
         if ($citysResult['error_code'] == 0) {
             return $citysResult;
+        } else {
+            return $citysResult['error_code'].":".$citysResult['reason'];
+        }
+    }
+
+    // 根据城市 ID 或城市名称获取天气
+    public function getWeatherByCity(Request $request)
+    {
+        $city = $request->city;
+        $cityWeatherResult = $this->weather->getWeather($city);
+
+        if ($cityWeatherResult['error_code'] == 0) {
+            $data = $cityWeatherResult['result'];
+            return $data;
+        } else {
+            return $cityWeatherResult['error_code'].":".$cityWeatherResult['reason'];
+        }
+    }
+
+    public function getWeatherByIP(Request $request)
+    {
+        $ip = $request->ip;
+        $ipWeatherResult = $this->weather->getWeatherByIP($ip);
+
+        if ($ipWeatherResult['error_code'] == 0) {
+            $data = $ipWeatherResult['result'];
+            return $data;
+        } else {
+            return $ipWeatherResult['error_code'].":".$ipWeatherResult['reason'];
+        }
+    }
+
+    // 根据 GPS 获取天气
+    public function getWeatherByGeo(Request $request)
+    {
+        $geoWeatherResult = $this->weather->getWeatherByGeo(116.401394, 39.916042);
+
+        if ($geoWeatherResult['error_code'] == 0) {    //以下可根据实际业务需求，自行改写
+            return $geoWeatherResult['result'];
+        } else {
+            return $geoWeatherResult['error_code'].":".$geoWeatherResult['reason'];
+        }
+    }
+
+    // 根据城市 ID 或城市名称获取三小时内的天气
+    public function getForecast(Request $request)
+    {
+        $forecastResult = $this->weather->getForecast($request->city);
+
+        if ($forecastResult['error_code'] == 0) {
+            return = $forecastResult['result'];
+        } else {
+            return $forecastResult['error_code'].":".$forecastResult['reason'];
         }
     }
 }
--- routes/api.php
+++ routes/api.php
@@ -3,7 +3,9 @@
 $api = app('Dingo\Api\Routing\Router');
 
 $api->version('v1', ['namespace' => "App\Http\Controllers\Api"], function($api) {
-    $api->get('weather', "WeatherController@getCitys");
+    $api->get('weather/citys', "WeatherController@getCitys");
+    $api->get('weather/city', "WeatherController@getWeatherByCity");
+    $api->get('weather/ip', "WeatherController@getWeatherByIP");
 });
 
 $api->version('v2', function($api) {
